277e13ed7a588a516f0513b093cf0d2002ef59b9
PranavX500 <ps5840432@gmail.com>
2025-11-29 23:48:50 +0530

    added login and signup logic and implement jwt

---
+++ Service/AuthUtil.java
@@ -0,0 +1,41 @@
+package com.example.User_service.Service;
+
+import com.example.User_service.Model.User;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.security.Keys;
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class AuthUtil {
+    @Value("${jwt.secretKey}")
+    private String jwtSecretKey;
+    public SecretKey getSecretKey() {
+        return Keys.hmacShaKeyFor(jwtSecretKey.getBytes(StandardCharsets.UTF_8));
+    }
+    public String generateAccessToken(User user){
+        return Jwts.builder()
+                .subject(user.getUsername())
+                .claim("role", user.getRole())
+                .claim("email", user.getEmailId())
+                .claim("userId",user.getId())// <-- ADD THIS
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 10))
+                .signWith(getSecretKey())
+                .compact();
+    }
+
+
+//    public String getUsernameFromToken(String token) {
+//        return Jwts.parser()
+//                .verifyWith(getSecretKey())
+//                .build()
+//                .parseSignedClaims(token)
+//                .getPayload()
+//                .getSubject();
+//    }
+
+}
